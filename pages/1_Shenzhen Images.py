83c3e3a6cfb8c6979ef3c50616d6364a1de982ac
2380363180a-del <2380363180a@gmail.com>
2026-03-30 16:43:34 +0800

    1

--- pages/1_Shenzhen Images.py
+++ pages/1_Shenzhen Images.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.write("""# Shenzhen in 2011""")
-st.write("""## Shenzhen CBD and River""")
+st.write(""" Shenzhen CBD and River""")
 st.image("pages\\Shenzhen_CBD_and_River.jpg")
