a5207a0e283a3230024b838e6c1505384e111dc4
2380363180a-del <2380363180a@gmail.com>
2026-03-30 21:18:09 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -4,6 +4,7 @@ st.write("""# Shenzhen 1980-2025 Urban Development Digital Archive""")
 
 st.markdown("---")
 
+st.image("Commercial_area_of_tutian_to_east2020.jpg")
 st.subheader("Introduction")
 st.write("This digital archive documents the **45-year transformation of Shenzhen** from a small border town to a global hub of innovation and urban development. Through historical photographs, official documents, urban maps, statistical charts, and cultural landmarks, it visualises the city’s rapid growth, industrial evolution, and spatial expansion.")
 st.write("Structured chronologically, the archive combines a scrollable timeline with an interactive map slider to illustrate how Shenzhen evolved from a special economic zone into a model of modern urbanisation.")
