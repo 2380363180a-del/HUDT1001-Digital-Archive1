c36ce4a00322d861049b2965f9d5f26201ffe834
2380363180a-del <2380363180a@gmail.com>
2026-03-31 19:01:58 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -43,10 +43,7 @@ import base64
 import json
 import streamlit.components.v1 as components
 
-st.subheader("Time Slider")
 st.title("Shenzhen: 45 Years of Transformation")
-st.divider()
-
 # 2. 准备数据
 available_years = [1979] + list(range(1984, 2021))
 
