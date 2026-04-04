091c9d5e5aaf35bb9a6291e2d6a175f652627a0e
2380363180a-del <2380363180a@gmail.com>
2026-04-04 18:46:13 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -41,7 +41,7 @@ def get_images_base64():
         else:
             img_dict[year] = None
     return img_dict
-with st.spinner("⏳ 正在打包历史影像，初次加载请稍候..."):
+with st.spinner("loading..."):
     images_b64 = get_images_base64()
     js_images = json.dumps(images_b64)
     js_captions = json.dumps({y: f"Shenzhen in the year {y}" for y in available_years})
@@ -69,7 +69,7 @@ html_code = f"""
     <div id="image-container">
         <img id="display-image" src="" alt="Shenzhen Archive">
         <div id="no-img-text" class="warning" style="display:none;">
-            📸 The image for this year is not yet available in the archive.
+             The image for this year is not yet available in the archive.
         </div>
     </div>
     
@@ -86,7 +86,6 @@ html_code = f"""
         const caption = document.getElementById('caption-display');
         const yearDisplay = document.getElementById('year-display');
 
-        // 更新视图的核心函数
         function updateView(index) {{
             const year = years[index];
             yearDisplay.innerText = year;
@@ -147,7 +146,6 @@ def display_object(year, title, description, image_path, source, license_text,
     st.markdown("---")
     st.markdown(f'<p class="light-text"><strong>Source:</strong> {source}</p>', unsafe_allow_html=True)
     st.markdown(f'<p class="light-text"><strong>License:</strong> {license_text}</p>', unsafe_allow_html=True)
-    
     st.markdown("---")   
     # ============================================
 display_object(
@@ -173,7 +171,7 @@ display_object(
     description="A construction site in Shenzhen 1982",
     image_path="Milestone Sources/1982_Shenzhen_in_1982.jpg",         
     source="https://en.wikipedia.org/wiki/File:Shenchen_in_1982.jpg",
-    license_text="CC BY-SA 3.0",
+    license_text="Creative Commons Attribution-Share Alike 3.0 Unported",
     caption="1982·Shenzhen",
     extra_sections=[
         ("Historical Significance", """In 1982, the slogan "Time is money, efficiency is life" was proposed in Shenzhen, 
@@ -187,7 +185,7 @@ display_object(
     description="The International Trade Building in Shenzhen, China",
     image_path="Milestone Sources/1985_Guomao_Building_2006.jpeg",         
     source="https://zh.wikipedia.org/wiki/File:SZITB.JPG",
-    license_text="CC BY-SA 2.5",
+    license_text="Creative Commons Attribution-Share Alike 2.5 Generic",
     caption="2006·Guomao Building",
     extra_sections=[
         ("Historical Significance", """Construction of the Guomao Building began in 1982 and was completed in just 37 months by 1985. 
@@ -202,7 +200,7 @@ display_object(
     description="The Shenzhen stock market",
     image_path="Milestone Sources/1990_Shenzhen_Stock_Exchange_2005.png",
     source="https://zh.wikipedia.org/wiki/File:Shenzhen_walk_02.JPG",
-    license_text="CC BY-SA 3.0",
+    license_text="Creative Commons Attribution-Share Alike 3.0 Unported",
     caption="2005·Shenzhen Stock Exchange",
     extra_sections=[
         ("Historical Significance", """The Shenzhen Stock Exchange was formally launched in 1990. 
@@ -214,194 +212,213 @@ display_object(
 # ============================================
 display_object(
     year="1991",
-    title="title",
-    description="description(must match Excel)...",
+    title="Shenzhen Bao’an International Airport Opening",
+    description="Shenzhen Airport Terminal A",
     image_path="Milestone Sources/1991_Shenzhen_Baoan_International_Airport_Opening.JPG",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
+    source="https://zh.wikipedia.org/wiki/File:Shenzhen_Airport_Terminal_A.JPG",
+    license_text="Creative Commons Attribution-Share Alike 4.0 International",
+    caption="1991·Shenzhen Bao’an International Airport Opening",
     extra_sections=[
-        ("Historical Significance", """historical significance...""")
+        ("Historical Significance", """Shenzhen Bao’an International Airport opened in 1991. 
+         This new aviation hub greatly improved the city’s global connectivity and supported its 
+         rapid economic integration with Hong Kong and the international market.""")
     ]
 )
 # ============================================
 display_object(
     year="1996",
-    title="title",
-    description="description(must match Excel)...",
+    title="Shun Hing Square Complete",
+    description="Shun Hing Square, Shenzhen, China, 2001",
     image_path="Milestone Sources/1996_Shun_Hing_Square_Complete.jpg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    source="https://commons.wikimedia.org/wiki/File:Shun_Hing_Square.jpg",
+    license_text="Creative Commons Attribution-ShareAlike 3.0 Unported and GNU Free Documentation License",
+    caption="1996·Shun Hing Square Complete",
+    extra_sections=[
+        ("Historical Significance", """Shun Hing Square (Di Wang Tower) was completed in 1996 at 384 meters. 
+         It became China’s tallest building at the time and symbolized Shenzhen’s economic rise while defining the Luohu Central Business District.""")
     ]
 )
 # ============================================
 display_object(
     year="1997",
-    title="title",
-    description="description(must match Excel)...",
-    image_path="Milestone Sources/1997_ ChinaSARHongKong_border.png",         
-    source="source link",
-    license_text="license statement",
-    caption="caption"
+    title="China-SAR-HongKong border",
+    description="Shenzhen & S.A.R. Hong Kong border - view from Shenzhen, Luo Hu District - July 2005",
+    image_path="Milestone Sources/1997_ ChinaSARHongKong_border.png",          
+    source="https://zh.wikipedia.org/wiki/File:China-SAR-HongKong_border_view-from-Shenzhen1.jpg",
+    license_text="Creative Commons CC0 1.0 Universal Public Domain Dedication",
+    caption="1997·China-SAR-HongKong border",
+    extra_sections=[
+        ("Historical Significance", """On 1 July 1997, Hong Kong was handed over to China under “One Country, Two Systems.” 
+         The smooth transition greatly boosted international confidence in Shenzhen, strengthening its role as the
+          mainland's gateway to global capital and investment.""")
+   ]
 )
 # ============================================
 display_object(
     year="1999",
-    title="title",
-    description="description(must match Excel)...",
-    image_path="Milestone Sources/1999_First_China_HiTech_Fair.jpg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    title="First China Hi-Tech Fair (CHTF)",
+    description="Shenzhen Convention and Exhibition Center seen from the Free Sky observation deck at Ping An Finance Centre in Shenzhen, China.",
+    image_path="Milestone Sources/1999_First_China_HiTech_Fair.jpg",             source="https://en.wikipedia.org/wiki/File:%E4%BA%91%E9%99%85%E8%A7%82%E5%85%89%E5%B1%82_2019_-_07.jpg",
+    license_text="Creative Commons CC0 1.0 Universal Public Domain Dedication.",
+    caption="1999·First China Hi-Tech Fair (CHTF)",
+    extra_sections=[
+        ("Historical Significance", """The first China Hi-Tech Fair was held in Shenzhen in 1999. 
+         It established the city as a national platform for technology exchange and innovation, 
+         attracting capital and startups while building Shenzhen’s reputation as a tech hub.""")
     ]
 )
 # ============================================
 display_object(
     year="2000",
-    title="title",
-    description="description(must match Excel)...",
+    title="Shenzhen University Town Founding",
+    description="University Town Shenzhen main entrance.",
     image_path="Milestone Sources/2000_Shenzhen_University_Town_Founding.jpg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    source="https://en.wikipedia.org/wiki/File:Umversitytownszmainentrance.jpg",
+    license_text="Creative Commons Attribution-Share Alike 3.0 Unported",
+    caption="2000·Shenzhen University Town Founding",
+    extra_sections=[
+        ("Historical Significance", """Shenzhen University Town was founded in 2000. 
+         It attracted top universities and research institutes, creating a strong local higher-education and R&D base 
+         that supported the city’s transition into a knowledge economy.""")
     ]
 )
 # ============================================
 display_object(
     year="2004",
-    title="title",
-    description="description(must match Excel)...",
+    title="Shenzhen Metro Line 1 Opening",
+    description="Train 126 of Shenzhen Metro Line 1",
     image_path="Milestone Sources/2004_Shenzhen_Metro_Line_1_Opening.jpeg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    source="https://commons.wikimedia.org/wiki/File:ShenzhenMetro-L1-126.jpeg",
+    license_text="Creative Commons Attribution-Share Alike 4.0 International (CC BY-SA 4.0) / GNU Free Documentation License, version 1.2 or later.",
+    caption="2004·Shenzhen Metro Line 1 Opening",
+    extra_sections=[
+        ("Historical Significance", """Shenzhen Metro Line 1 opened in 2004. This marked the beginning of the city’s modern rail 
+         transit era and formed the backbone of its urban transportation network.""")
     ]
 )
 # ============================================
 display_object(
     year="2006",
-    title="title",
-    description="description(must match Excel)...",
+    title="DJI",
+    description="DJI Innovations logo. Extracted from a DJI Quick Start Guide.",
     image_path="Milestone Sources/2006_DJI.png",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    source="https://en.wikipedia.org/wiki/File:DJI_Innovations_logo.svg",
+    license_text="Public Domain",
+    caption="2006·DJI Innovations logo",
+    extra_sections=[
+        ("Historical Significance", """DJI (Da-Jiang Innovations) was founded in a small apartment in Shenzhen in 2006. 
+         The company grew into the global leader in drones, establishing Shenzhen’s worldwide reputation for hardware innovation and entrepreneurship.""")
     ]
 )
 # ============================================
 display_object(
     year="2010",
-    title="title",
-    description="description(must match Excel)...",
-    image_path="Milestone Sources/2010_SEZ_Expansion_to_Entire_City.png",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    title="SEZ Expansion to Entire City",
+    description="Subdivision of Shenzhen (French version). A map showing the administrative districts of Shenzhen as of the end of 2009, with labels in French.",
+    image_path="Milestone Sources/2010_SEZ_Expansion_to_Entire_City.png",            source="https://commons.wikimedia.org/wiki/File:Shenzhen_administrative_divisions_(end_2009,_fr).svg",
+    license_text="Creative Commons Attribution-Share Alike 2.5 Generic (CC BY-SA 2.5).",
+    caption="2010·SEZ Expansion to Entire City",
+    extra_sections=[
+        ("Historical Significance", """In 2010, the Special Economic Zone was expanded to cover the entire city (from 395 km² to 1,953 km²). 
+         This major policy change enabled unified urban planning and city-wide integrated development.""")
     ]
 )
 # ============================================
 display_object(
     year="2011",
-    title="title",
-    description="description(must match Excel)...",
+    title="Kingkey 100 Completion",
+    description="A photograph of the Kingkey 100 Tower in Shenzhen, China.",
     image_path="Milestone Sources/2011_Kingkey_100_Completion.jpg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    source="https://en.wikipedia.org/wiki/File:Kingkey-100-5.jpg",
+    license_text="Creative Commons CC0 1.0 Universal Public Domain Dedication.",
+    caption="2011·Kingkey 100 Completion",
+    extra_sections=[
+        ("Historical Significance", """Kingkey 100 (441.8 m) was completed in 2011. 
+         It became a landmark of Futian CBD and demonstrated Shenzhen's growing financial and economic strength.""")
     ]
 )
 # ============================================
 display_object(
     year="2014",
-    title="title",
-    description="description(must match Excel)...",
+    title="Ping An Finance Center Topping Out",
+    description="Centre commercial area of Futian district",
     image_path="Milestone Sources/2014_Ping_An_Finance_Center_Topping_Out.jpg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    source="https://commons.wikimedia.org/wiki/File:Commercial_area_of_futian_to_east2020_(4to3).jpg",
+    license_text="Creative Commons Attribution-Share Alike 4.0 International (CC BY-SA 4.0).",
+    caption="2014·Ping An Finance Center Topping Out",
+    extra_sections=[
+        ("Historical Significance", """Ping An Finance Center (599.1 m) was topped out in 2014. 
+         It became Shenzhen's new skyline icon and significantly enhanced the city's global image as a modern metropolis.""")
     ]
 )
 # ============================================
 display_object(
     year="2016",
-    title="title",
-    description="description(must match Excel)...",
+    title="Shenzhen-Hong Kong Stock Connect Open",
+    description="An interior view photograph of the Shenzhen Stock Exchange.",
     image_path="Milestone Sources/2016_ShenzhenHongKong_Stock_Connect_Open.png",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    source="https://commons.wikimedia.org/wiki/File:Shenzhen_Stock_Exchange.jpg",
+    license_text="Creative Commons Attribution-Share Alike 3.0 Unported",
+    caption="2016·Shenzhen-Hong Kong Stock Connect Open",
+    extra_sections=[
+        ("Historical Significance", """Shenzhen-Hong Kong Stock Connect was launched in 2016. 
+         It deepened financial integration between Shenzhen and Hong Kong, further strengthening the city’s position as a regional financial hub.""")
     ]
 )
 # ============================================
 display_object(
     year="2018",
-    title="title",
-    description="description(must match Excel)...",
+    title="Hong Kong-Zhuhai-Macao Bridge",
+    description="Taken from a TurboJET after leaving Macau.",
     image_path="Milestone Sources/2018_HongKongZhuhaiMacao_Bridge.jpeg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Historical Significance", """historical significance...""")
+    source="https://commons.wikimedia.org/wiki/File:West_section_of_Hong_Kong-Zhuhai-Macau_Bridge_(20180902174105).jpg",
+    license_text="Creative Commons Attribution-Share Alike 4.0",
+    caption="2018·Hong Kong-Zhuhai-Macao Bridge",
+    extra_sections=[
+        ("Historical Significance", """The Hong Kong-Zhuhai-Macao Bridge was opened in 2018. 
+         While bypassing the city directly, it accelerated the broader economic integration of the Greater Bay Area, driving Shenzhen to expedite the Shenzhen-Zhongshan Link and further solidifying its competitive edge as a core regional transport and innovation hub.""")
     ]
 )
 # ============================================
 display_object(
     year="2023",
-    title="title",
-    description="description(must match Excel)...",
+    title="Intelligent Connected Vehicles Regulation",
+    description="Regulations of Shenzhen Special Economic Zone on the Administration of Intelligent Connected Vehicles",
     image_path="Milestone Sources/2023_Intelligent_Connected_Vehicles_Regulation.png",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
-        extra_sections=[
-        ("Translation", """translation"""),
-        
-        ("Historical Significance", """historical significance...""")
+    source="https://sf.sz.gov.cn/fggzywyb/content/mpost_11216374.html",
+    license_text="Public information provided by the Shenzhen Municipal Government; Subject to Chinese government information disclosure and copyright laws",
+    caption="2023·Intelligent Connected Vehicles Regulation",
+    extra_sections=[
+        ("Historical Significance", """Shenzhen issued China’s first local regulation on Intelligent Connected Vehicles in 2023.
+         It pioneered autonomous driving legislation and positioned the city as a global leader in future mobility industries.""")
     ]
 )
 # ============================================
 display_object(
     year="2024",
-    title="title",
-    description="description(must match Excel)...",
+    title="Shenzhen-Zhongshan Link Opening",
+    description="Shenzhen-Zhongshan Link in June 2025",
     image_path="Milestone Sources/2024_ShenzhenZhongshan_Link_Opening.jpg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
+    source="https://en.wikipedia.org/wiki/File:Shenzhen-Zhongshan_Link_2025.06.jpg",
+    license_text="Creative Commons Attribution-Share Alike 4.0 International",
+    caption="2024·Shenzhen-Zhongshan Link Opening",
     extra_sections=[
-        ("Historical Significance", """historical significance...""")
+        ("Historical Significance", """The Shenzhen-Zhongshan Link opened in June 2024. 
+         This 24 km mega-bridge strengthened connectivity with western Guangdong and accelerated integration within the Greater Bay Area.""")
     ]
 )
 # ============================================
 display_object(
     year="2025",
-    title="title",
-    description="description(must match Excel)...",
+    title="Qianhai Huafa Ice & Snow World Opening",
+    description="The design of Shenzhen Qianhai Huafa Snow World by 10 Design",
     image_path="Milestone Sources/2025_Qianhai_Huafa_Ice_Snow_World_Opening.jpg",         
-    source="source link",
-    license_text="license statement",
-    caption="caption",
+    source="https://commons.wikimedia.org/wiki/File:GD_%E5%BB%A3%E6%9D%B1_Guangdong_%E8%B7%A8%E5%B8%82%E5%A4%A7%E5%B7%B4_intercity_bus_tour_view_%E6%B7%B1%E5%9C%B3_Shenzhen_%E5%AF%B6%E5%AE%89%E5%8D%80_BaoAn_District_buildings_1123am_October_2024_R12S_436.jpg",
+    license_text="Copyright © 10 Design. All rights reserved.",
+    caption="2025·Qianhai Huafa Ice & Snow World Opening",
     extra_sections=[
-        ("Historical Significance", """historical significance...""")
+        ("Historical Significance", """Qianhai Ice & Snow World (430,000 m²) opened in 2025. 
+         It boosted cultural and tourism development while upgrading Shenzhen’s urban service functions and quality of life.""")
     ]
 )
+
